597dfb548a1a171645c112155a12582137140abe
jeidiidx <jeid.ddr@gmail.com>
2019-01-02 22:40:26 +0900

    バリデーション

--- product/app/models/member.rb
+++ product/app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+	validates :name, presence:true
+	validates :email, presence:true
+	validates :password, presence:true
+	
 end
--- product/config/routes.rb
+++ product/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'session/new'
   resources :members
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
